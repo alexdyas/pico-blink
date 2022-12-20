53c07dd3051d1b652ebe9fbd98608ad5af019ad7
Alexander Dyas <me@alexdyas.com>
2022-12-20 16:19:18 +0100

    First commit

---
+++ blink.c
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+    Added comments
+
+    https://www.electronicshub.org/program-raspberry-pi-pico-using-c/
+
+*/
+
+#include "pico/stdlib.h"
+
+int main() {
+    const uint LED_PIN = 25; /* On-board LED. Connected to GPIO 25 */
+    gpio_init(LED_PIN); /* Initialize GPIO function on the Pin */
+    gpio_set_dir(LED_PIN, GPIO_OUT); /* Set the Pin as Output */
+    while (true) {
+        gpio_put(LED_PIN, 1); /* Make the GPIO Pin HIGH */
+        sleep_ms(250); /* Delay of 250ms */
+        gpio_put(LED_PIN, 0); /* Make the GPIO Pin LOW */
+        sleep_ms(250); /* Delay of 250ms */
+    }
+}
---
+++ run_build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Name of directory to store build artifacts
+BUILD_DIR="build"
+
+# Set this so pico_sdk_import.cmake can see the Pico SDK library path
+export PICO_SDK_PATH="../../pico-sdk"
+
+# Remove build dir <- Probably don't really need this, but cleaner for now
+if [ -d "${BUILD_DIR}" ]
+then
+  echo "---------- Removing existing build directory ------------"
+  rm -rf "${BUILD_DIR}"
+fi
+
+# Construct the make files
+echo "---------- cmake ------------"
+cmake -B "${BUILD_DIR}" -S .
+
+# Compile
+echo "---------- make ------------"
+make -C "${BUILD_DIR}"/
